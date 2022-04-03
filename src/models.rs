3b83882693383d6f9adeb8faa1b9148fedc750e2
fox0 <red-fox0@yandex.ru>
2022-04-03 15:08:42 +0700

    #[serde(tag = "Action")]

--- src/models.rs
+++ src/models.rs
@@ -46,15 +46,37 @@ pub struct OrgDirection {
 // }
 
 
-// #[allow(dead_code)]
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub enum Action {
-    Add,
-    Remove,
-    Edit,
-    GetMessage,
-    MessageConfirm,
-    Get,
+// #[derive(Debug, PartialEq, Serialize, Deserialize)]
+// pub enum Action {
+//     Add,
+//     Remove,
+//     Edit,
+//     GetMessage,
+//     MessageConfirm,
+//     Get,
+// }
+
+#[derive(Serialize)]
+#[serde(tag = "Action")]
+pub enum Header {
+    // воспользуемся особенностью, что лишние поля в json игнорируются
+    // #[serde(skip_deserializing)]
+    #[serde(rename_all = "PascalCase")]
+    CheckCertificate { ogrn: String, kpp: String },
+
+    // #[serde(skip_deserializing)]
+    #[serde(rename_all = "PascalCase")]
+    GetMessage { ogrn: String, kpp: String, id_jwt: u64 },
+
+    // #[serde(skip_deserializing)]
+    #[serde(rename_all = "PascalCase")]
+    MessageConfirm { ogrn: String, kpp: String, id_jwt: u64 },
+
+    // #[serde(skip_deserializing)]
+    #[serde(rename_all = "PascalCase")]
+    Add { ogrn: String, kpp: String, entity_type: String },
+
+    //Response { id_jwt: u64, entity_type: String, action: Action, payload_type: PayloadType },
 }
 
 #[derive(Debug, PartialEq, Deserialize)]
@@ -63,92 +85,46 @@ pub enum PayloadType {
     Error,
 }
 
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct Header {
-    #[serde(skip_deserializing)]
-    ogrn: String,
-    #[serde(skip_deserializing)]
-    kpp: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    entity_type: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    action: Option<Action>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    id_jwt: Option<u64>,
-    #[serde(skip_serializing)]
-    payload_type: Option<PayloadType>,
-}
-
-
-pub struct Token<T>
-    where T: Serialize {
+pub struct Token<T> where T: Serialize {
     header: Header,
     payload: Option<T>,
 }
 
-impl<T> Token<T>
-    where T: Serialize {
+impl<T> Token<T> where T: Serialize {
     /// Проверка действительности регистрации сертификата к организации
     ///
     /// ```
     /// let token: Token<NoPayload> = Token::new_check_certificate(ogrn, kpp);
     /// ```
     pub fn new_check_certificate(ogrn: String, kpp: String) -> Self {
-        let header = Header {
-            ogrn,
-            kpp,
-            entity_type: None,
-            action: None,
-            id_jwt: None,
-            payload_type: None,
-        };
-        Self { header, payload: None }
-    }
-
-    pub fn new_add(ogrn: String, kpp: String, payload: T) -> Self {
-        Self::new_add_or_edit(ogrn, kpp, payload, Action::Add)
-    }
-
-    pub fn new_edit(ogrn: String, kpp: String, payload: T) -> Self {
-        Self::new_add_or_edit(ogrn, kpp, payload, Action::Edit)
-    }
-
-    fn new_add_or_edit(ogrn: String, kpp: String, payload: T, action: Action) -> Self {
-        let header = Header {
-            ogrn,
-            kpp,
-            entity_type: Some(Self::get_entity_type(&payload)),
-            action: Some(action),
-            id_jwt: None,
-            payload_type: None,
-        };
-        Self { header, payload: Some(payload) }
+        Self {
+            header: Header::CheckCertificate { ogrn, kpp },
+            payload: None,
+        }
     }
 
     /// Запрос на получение сообщений из очереди
     pub fn new_get_message(ogrn: String, kpp: String, id_jwt: Option<u64>) -> Self {
-        let header = Header {
-            ogrn,
-            kpp,
-            entity_type: None,
-            action: Some(Action::GetMessage),
-            id_jwt: Some(id_jwt.unwrap_or(0u64)),
-            payload_type: None,
-        };
-        Self { header, payload: None }
+        let id_jwt = id_jwt.unwrap_or(0u64);
+        Self {
+            header: Header::GetMessage { ogrn, kpp, id_jwt },
+            payload: None,
+        }
     }
 
     pub fn new_message_confirm(ogrn: String, kpp: String, id_jwt: u64) -> Self {
-        let header = Header {
-            ogrn,
-            kpp,
-            entity_type: None,
-            action: Some(Action::MessageConfirm),
-            id_jwt: Some(id_jwt),
-            payload_type: None,
-        };
-        Self { header, payload: None }
+        Self {
+            header: Header::MessageConfirm { ogrn, kpp, id_jwt },
+            payload: None,
+        }
+    }
+
+    pub fn new_add(ogrn: String, kpp: String, payload: T) -> Self {
+        let entity_type = Self::get_entity_type(&payload);
+        Self {
+            header: Header::Add { ogrn, kpp, entity_type },
+            payload: Some(payload),
+        }
     }
 
     fn get_entity_type(payload: &T) -> String {
@@ -161,10 +137,8 @@ impl<T> Token<T>
     }
 }
 
-impl<T> Serialize for Token<T>
-    where T: Serialize {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where S: Serializer {
+impl<T> Serialize for Token<T> where T: Serialize {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
         let header = serde_json::to_string(&self.header).unwrap();
         let header = base64::encode(header);
 
@@ -192,7 +166,7 @@ impl<T> Serialize for Token<T>
 
 #[cfg(test)]
 mod tests {
-    use crate::models::{NoPayload, Header, OrgDirection, Token, Action, PayloadType};
+    use crate::models::{NoPayload, OrgDirection, Token};
 
     #[test]
     fn org_direction_to_xml() {
@@ -202,43 +176,27 @@ mod tests {
     }
 
     #[test]
-    fn header_to_json() {
-        let header = Header {
-            ogrn: "123".to_string(),
-            kpp: "456".to_string(),
-            entity_type: None,
-            action: None,
-            id_jwt: None,
-            payload_type: None,
-        };
-        let json = serde_json::to_string(&header).unwrap();
-        assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456"}"#);
-    }
-
-    #[test]
-    fn header_from_json() {
-        let json = r#"
-{
-  "IdJwt": 42,
-  "PayloadType": "Success",
-  "EntityType": "OrgDirection",
-  "Action": "Edit"
-}
-"#;
-        let header: Header = serde_json::from_str(json).unwrap();
-        assert_eq!(header.id_jwt, Some(42));
-        assert_eq!(header.entity_type, Some("OrgDirection".to_string()));
-        assert_eq!(header.action, Some(Action::Edit));
-        assert_eq!(header.payload_type, Some(PayloadType::Success));
-    }
-
-    #[test]
     fn token_new_check_certificate() {
         let token: Token<NoPayload> = Token::new_check_certificate("123".to_string(), "456".to_string());
         let json = serde_json::to_string(&token.header).unwrap();
-        assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456"}"#);
+        // assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456"}"#);
+        assert_eq!(json, r#"{"Action":"CheckCertificate","Ogrn":"123","Kpp":"456"}"#);
         let json = serde_json::to_string(&token).unwrap();
-        assert_eq!(json, r#"{"Token":"eyJPZ3JuIjoiMTIzIiwiS3BwIjoiNDU2In0=..AQID"}"#);
+        assert_eq!(json, r#"{"Token":"eyJBY3Rpb24iOiJDaGVja0NlcnRpZmljYXRlIiwiT2dybiI6IjEyMyIsIktwcCI6IjQ1NiJ9..AQID"}"#);
+    }
+
+    #[test]
+    fn token_new_get_message() {
+        let token: Token<NoPayload> = Token::new_get_message("123".to_string(), "456".to_string(), None);
+        let json = serde_json::to_string(&token.header).unwrap();
+        assert_eq!(json, r#"{"Action":"GetMessage","Ogrn":"123","Kpp":"456","IdJwt":0}"#);
+    }
+
+    #[test]
+    fn token_new_message_confirm() {
+        let token: Token<NoPayload> = Token::new_message_confirm("123".to_string(), "456".to_string(), 42);
+        let json = serde_json::to_string(&token.header).unwrap();
+        assert_eq!(json, r#"{"Action":"MessageConfirm","Ogrn":"123","Kpp":"456","IdJwt":42}"#);
     }
 
     #[test]
@@ -246,24 +204,27 @@ mod tests {
         let payload = OrgDirection { uid: "123".to_string(), id_direction: 42 };
         let token = Token::new_add("123".to_string(), "456".to_string(), payload);
         let json = serde_json::to_string(&token.header).unwrap();
-        assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456","EntityType":"OrgDirection","Action":"Add"}"#);
+        assert_eq!(json, r#"{"Action":"Add","Ogrn":"123","Kpp":"456","EntityType":"OrgDirection"}"#);
         let xml = serde_xml_rs::to_string(&token.payload).unwrap();
         assert_eq!(xml, r"<OrgDirection><Uid>123</Uid><IdDirection>42</IdDirection></OrgDirection>");
         let json = serde_json::to_string(&token).unwrap();
-        assert_eq!(json, r#"{"Token":"eyJPZ3JuIjoiMTIzIiwiS3BwIjoiNDU2IiwiRW50aXR5VHlwZSI6Ik9yZ0RpcmVjdGlvbiIsIkFjdGlvbiI6IkFkZCJ9.PFBhY2thZ2VEYXRhPjxPcmdEaXJlY3Rpb24+PFVpZD4xMjM8L1VpZD48SWREaXJlY3Rpb24+NDI8L0lkRGlyZWN0aW9uPjwvT3JnRGlyZWN0aW9uPjwvUGFja2FnZURhdGE+.AQID"}"#);
+        assert_eq!(json, r#"{"Token":"eyJBY3Rpb24iOiJBZGQiLCJPZ3JuIjoiMTIzIiwiS3BwIjoiNDU2IiwiRW50aXR5VHlwZSI6Ik9yZ0RpcmVjdGlvbiJ9.PFBhY2thZ2VEYXRhPjxPcmdEaXJlY3Rpb24+PFVpZD4xMjM8L1VpZD48SWREaXJlY3Rpb24+NDI8L0lkRGlyZWN0aW9uPjwvT3JnRGlyZWN0aW9uPjwvUGFja2FnZURhdGE+.AQID"}"#);
     }
 
-    #[test]
-    fn token_new_get_message() {
-        let token: Token<NoPayload> = Token::new_get_message("123".to_string(), "456".to_string(), None);
-        let json = serde_json::to_string(&token.header).unwrap();
-        assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456","Action":"GetMessage","IdJwt":0}"#);
-    }
-
-    #[test]
-    fn token_new_message_confirm() {
-        let token: Token<NoPayload> = Token::new_message_confirm("123".to_string(), "456".to_string(), 42);
-        let json = serde_json::to_string(&token.header).unwrap();
-        assert_eq!(json, r#"{"Ogrn":"123","Kpp":"456","Action":"MessageConfirm","IdJwt":42}"#);
-    }
+//     #[test]
+//     fn header_from_json() {
+//         let json = r#"
+// {
+//   "IdJwt": 42,
+//   "PayloadType": "Success",
+//   "EntityType": "OrgDirection",
+//   "Action": "Edit"
+// }
+// "#;
+//         let header: Header = serde_json::from_str(json).unwrap();
+//         assert_eq!(header.id_jwt, Some(42));
+//         assert_eq!(header.entity_type, Some("OrgDirection".to_string()));
+//         assert_eq!(header.action, Some(Action::Edit));
+//         assert_eq!(header.payload_type, Some(PayloadType::Success));
+//     }
 }
