8e62eacdf938d612200a362b6b3b4101a306f910
fox0 <red-fox0@yandex.ru>
2022-05-21 00:42:01 +0700

    yaserde

--- src/models.rs
+++ src/models.rs
@@ -1,67 +1,49 @@
-use serde::Serialize;
-use ss_derive::PackageData;
+#![allow(non_snake_case)]
+
+use yaserde_derive::YaSerialize;
+use validator::Validate;
 
 
-/// ```
-/// #[derive(Serialize, PackageData)]
-/// #[serde(rename_all = "PascalCase")]
-/// pub struct OrgDirection {
-///     pub uid: String,
-///     //...
-/// }
-/// ```
-pub trait PackageData: Serialize {
-    fn get_name(&self) -> &str;
-    fn get_uid(&self) -> &String;
-
-    fn get_xml_add(&self) -> String {
-        let xml = serde_xml_rs::to_string(&self).unwrap();
-        format!(r#"<?xml version="1.0" encoding="utf-8"?><PackageData>{}</PackageData>"#, xml)
-    }
-
-    fn get_xml_remove(&self) -> String {
-        let name = self.get_name();
-        let uid = self.get_uid();
-        format!(r#"<?xml version="1.0" encoding="utf-8"?><PackageData><{}><Uid>{}</Uid></{}></PackageData>"#, name, uid, name)
-    }
+#[derive(YaSerialize)]
+pub enum PackageData {
+    OrgDirection(OrgDirection),
+    TargetOrganization(TargetOrganization),
 }
 
-#[derive(Serialize)]
-pub struct NoPayload;
-
-impl PackageData for NoPayload {
-    fn get_name(&self) -> &str {
-        unreachable!()
-    }
-    fn get_uid(&self) -> &String {
-        unreachable!()
-    }
-}
+//DirectionParamsValue
 
 /// Направления подготовки ООВО
-#[derive(Serialize, PackageData)]
-#[serde(rename_all = "PascalCase")]
+#[derive(YaSerialize, Validate)]
 pub struct OrgDirection {
-    pub uid: String,
-    pub id_direction: u64,
+    #[validate(length(max = 36))]
+    pub Uid: String,
+    pub IdDirection: u64,
+    //DirectionParamsValueList
 }
 
-
-#[cfg(test)]
-mod tests {
-    use crate::models::{OrgDirection, PackageData};
-
-    #[test]
-    fn org_direction_add() {
-        let payload = OrgDirection { uid: "123".to_string(), id_direction: 42 };
-        let xml = payload.get_xml_add();
-        assert_eq!(xml, r#"<?xml version="1.0" encoding="utf-8"?><PackageData><OrgDirection><Uid>123</Uid><IdDirection>42</IdDirection></OrgDirection></PackageData>"#);
-    }
-
-    #[test]
-    fn org_direction_remove() {
-        let payload = OrgDirection { uid: "123".to_string(), id_direction: 42 };
-        let xml = payload.get_xml_remove();
-        assert_eq!(xml, r#"<?xml version="1.0" encoding="utf-8"?><PackageData><OrgDirection><Uid>123</Uid></OrgDirection></PackageData>"#);
-    }
+/// Целевые организации
+#[derive(YaSerialize, Validate)]
+pub struct TargetOrganization {
+    #[validate(length(max = 36))]
+    pub Uid: String,
+    #[validate(length(equal = 13))]
+    pub Ogrn: String,
+    #[validate(length(equal = 9))]
+    pub Kpp: String,
+    #[validate(length(equal = 10))]
+    pub Inn: String,
+    #[validate(length(max = 255))]
+    pub ShortTitle: String,
+    #[validate(length(max = 500))]
+    pub FullTitle: String,
+    #[validate(length(max = 2048))]
+    pub Address: Option<String>,
+    #[validate(length(max = 255))]
+    pub Phone: Option<String>,
+    #[validate(length(max = 255))]
+    pub Email: Option<String>,
+    /// ФИО руководителя организации
+    #[validate(length(max = 500))]
+    pub ChiefNames: Option<String>,
 }
+
--- tests/models.rs
+++ tests/models.rs
@@ -5,8 +5,106 @@ mod models;
 
 #[cfg(test)]
 mod tests {
-    // #[test]
-    // fn test1() {
-    //     assert_eq!("", "");
-    // }
+    use validator::Validate;
+    use crate::models::{OrgDirection, PackageData, TargetOrganization};
+
+    fn get_config() -> yaserde::ser::Config {
+        yaserde::ser::Config {
+            perform_indent: true,
+            write_document_declaration: false,
+            indent_string: Some("    ".to_string()),
+            // ..Default::default()
+        }
+    }
+
+    #[test]
+    fn org_direction() {
+        let obj = OrgDirection {
+            Uid: "32342".to_string(),
+            IdDirection: 42,
+        };
+        obj.validate().unwrap();
+        let model = PackageData::OrgDirection(obj);
+
+        let config = get_config();
+        let xml = yaserde::ser::to_string_with_config(&model, &config).ok().unwrap();
+        // println!("{}", &xml);
+        assert_eq!(xml, "\
+<PackageData>
+    <OrgDirection>
+        <Uid>32342</Uid>
+        <IdDirection>42</IdDirection>
+    </OrgDirection>
+</PackageData>");
+    }
+
+    #[test]
+    fn target_organization() {
+        let obj = TargetOrganization {
+            Uid: "32342".to_string(),
+            Ogrn: "1234567800013".to_string(),
+            Kpp: "123456789".to_string(),
+            Inn: "1234567810".to_string(),
+            ShortTitle: "short".to_string(),
+            FullTitle: "full".to_string(),
+            Address: None,
+            Phone: None,
+            Email: None,
+            ChiefNames: None,
+        };
+        obj.validate().unwrap();
+        let model = PackageData::TargetOrganization(obj);
+
+        let config = get_config();
+        let xml = yaserde::ser::to_string_with_config(&model, &config).ok().unwrap();
+        // println!("{}", &xml);
+        assert_eq!(xml, "\
+<PackageData>
+    <TargetOrganization>
+        <Uid>32342</Uid>
+        <Ogrn>1234567800013</Ogrn>
+        <Kpp>123456789</Kpp>
+        <Inn>1234567810</Inn>
+        <ShortTitle>short</ShortTitle>
+        <FullTitle>full</FullTitle>
+    </TargetOrganization>
+</PackageData>");
+    }
+
+    #[test]
+    fn target_organization2() {
+        let obj = TargetOrganization {
+            Uid: "32342".to_string(),
+            Ogrn: "1234567800013".to_string(),
+            Kpp: "123456789".to_string(),
+            Inn: "1234567810".to_string(),
+            ShortTitle: "short".to_string(),
+            FullTitle: "full".to_string(),
+            Address: Some("11".to_string()),
+            Phone: Some("22".to_string()),
+            Email: Some("33".to_string()),
+            ChiefNames: Some("44".to_string()),
+        };
+        obj.validate().unwrap();
+        let model = PackageData::TargetOrganization(obj);
+
+        let config = get_config();
+        let xml = yaserde::ser::to_string_with_config(&model, &config).ok().unwrap();
+        // println!("{}", &xml);
+        assert_eq!(xml, "\
+<PackageData>
+    <TargetOrganization>
+        <Uid>32342</Uid>
+        <Ogrn>1234567800013</Ogrn>
+        <Kpp>123456789</Kpp>
+        <Inn>1234567810</Inn>
+        <ShortTitle>short</ShortTitle>
+        <FullTitle>full</FullTitle>
+        <Address>11</Address>
+        <Phone>22</Phone>
+        <Email>33</Email>
+        <ChiefNames>44</ChiefNames>
+    </TargetOrganization>
+</PackageData>");
+    }
 }
